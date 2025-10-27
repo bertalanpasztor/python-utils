55b97d301db0126e3f734a417753f581aed04f14
PASZTORBERTALAN\bertalan.pasztor <bertalan.pasztor@icellmobilsoft.hu>
2025-10-27 14:24:04 +0100

    közös munka előtt

--- STATION_X_DEVICE_X_DEVICE_UNIT/main.py
+++ STATION_X_DEVICE_X_DEVICE_UNIT/main.py
@@ -3,4 +3,5 @@ def get_distinct_eszkoz_kod_from_sheet():
 
 
 if __name__ == '__main__':
-    get_distinct_eszkoz_kod_from_sheet()
+	sheet_link = "https://docs.google.com/spreadsheets/d/1qabElQ2bv1n1HwhSnKsqcPD6Aw9H06Xq/edit?gid=716677592#gid=716677592"
+    eszkozlista = get_distinct_eszkoz_kod_from_sheet(sheet_link)
